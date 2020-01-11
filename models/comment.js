9dcd2935fc12e37530a6762a5e4e95bbfb03a879
Hayden Wampach <wampachhayden@gmail.com>
2020-01-10 18:00:46 -0800

    completed setup of comment.js file

---
+++ models/comment.js
@@ -0,0 +1,19 @@
+var mongoose = rquire("mongoose");
+
+var Schema = mongoose.Schema;
+
+var CommentSchema = new Schema({
+    title: {
+        type: String
+    
+    },
+   body: {
+       type: String,
+       required: true,
+   }
+
+});
+
+var Comment = mongoose.model("Comment, CommentSchema");
+
+module.exports = Comment;
