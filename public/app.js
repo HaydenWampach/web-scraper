de0c501d9acc3ea6f86a17ee05a007de2609743a
Hayden Wampach <wampachhayden@gmail.com>
2020-01-15 18:12:54 -0800

    added article save to app.js file

---
+++ public/app.js
@@ -0,0 +1,13 @@
+$(document).on("click", ".saveButton", function handleArticleSave() {
+    var articleToSave = $(this).parents(".panel").data();
+    articleToSave.saved = true;
+
+    $.ajax({
+        method: "PATCH",
+        url: "/api/articles",
+        data: articleToSave
+    })
+    .then(function(data) {
+        if (data.ok){}
+    })
+});
